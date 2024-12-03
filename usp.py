fdd0a9336526509b0b511a4166fb081b37a051fa
LiMoricz <Lianamoricz@gmail.com>
2024-12-03 04:41:03 +0000

    amm file

--- usp.py
+++ usp.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.interpolate import griddata
 
-def generate_heatmaps(file_path):
-    # Load the data from the text file
+def generateheatmaps(file_path):
+    #data from the text file
     data = pd.read_csv(file_path)
     data.columns = data.columns.str.strip()
     x = data['x']
@@ -13,6 +13,7 @@ def generate_heatmaps(file_path):
     min_values = data['min']
     grid_x, grid_y = np.mgrid[0:x.max():100j, 0:y.max():100j]
     grid_min_values = griddata((x, y), min_values, (grid_x, grid_y), method='cubic')
+
 
     # average time spent in min
     plt.figure(figsize=(10, 8))
@@ -25,12 +26,14 @@ def generate_heatmaps(file_path):
     plt.savefig('2spatial_gradient_average_time.png')
     plt.show()
 
+
     # density 
     density = data.groupby(['x', 'y']).size().reset_index(name='count')
     x_density = density['x']
     y_density = density['y']
     count_values = density['count']
     grid_density_values = griddata((x_density, y_density), count_values, (grid_x, grid_y), method='cubic')
+
 
     # heatmap for density
     plt.figure(figsize=(10, 8))
@@ -43,6 +46,9 @@ def generate_heatmaps(file_path):
     plt.savefig('2spatial_gradient_density.png')
     plt.show()
 
+
+
+
 if __name__ == "__main__":
     file_path = "uspfile.txt"
-    generate_heatmaps(file_path)
+    generateheatmaps(file_path)
